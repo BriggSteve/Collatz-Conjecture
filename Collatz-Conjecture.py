1be0011608a97b9ad0086bcf83877eed38544af5
SeniorIntern <52074393+SeniorIntern@users.noreply.github.com>
2019-07-11 09:27:33 -0700

    Create Collatz-Conjecture.py

---
+++ Collatz-Conjecture.py
@@ -0,0 +1,16 @@
+def variable_count():
+    variable = 0
+    while 1:
+        variable = variable + 1
+        yield variable
+        print("{0} has been tested".format(variable))
+
+
+def ccs():
+    for i in variable_count():
+        while i != 1:
+            if i % 2 == 0:
+                i = i / 2
+            else:
+                i = i * 3 + 1
+
