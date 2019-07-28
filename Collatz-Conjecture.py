60deeb0d094462347778f8cad62a641c399247d4
StevenBenentt <52074393+StevenBenentt@users.noreply.github.com>
2019-07-28 08:32:58 -0700

    Update Collatz-Conjecture.py
    Removed needless spaces

--- Collatz-Conjecture.py
+++ Collatz-Conjecture.py
@@ -1,6 +1,5 @@
 import itertools
 
-
 for i in itertools.count(1):
     print("{0} is being tested".format(i))
     while i != 1:
@@ -8,4 +7,3 @@ for i in itertools.count(1):
             i = i / 2
         else:
             i = i * 3 + 1
-
