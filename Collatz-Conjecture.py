56d95599297d72bee9dea3a7ff7c0ce1c9e692e9
StevenBenentt <52074393+StevenBenentt@users.noreply.github.com>
2019-07-28 08:32:38 -0700

    Update Collatz Conjecture
    Significantly improve Collatz conjecture(removed need for functions, added clearer print statement, removed janky code) after learning of itertools.count while creating Goldbach Conjecture Generator in python.

--- Collatz-Conjecture.py
+++ Collatz-Conjecture.py
@@ -1,17 +1,11 @@
-def variable_count():
-    variable = 0
-    while 1:
-        variable = variable + 1
-        yield variable
-        print("{0} has been tested".format(variable))
+import itertools
 
 
-def ccs():
-    for i in variable_count():
-        while i != 1:
-            if i % 2 == 0:
-                i = i / 2
-            else:
-                i = i * 3 + 1
+for i in itertools.count(1):
+    print("{0} is being tested".format(i))
+    while i != 1:
+        if i % 2 == 0:
+            i = i / 2
+        else:
+            i = i * 3 + 1
 
-ccs()
