2b236fca4eede02e39d8cf6febf33762b9701712
StevenBenentt <52074393+StevenBenentt@users.noreply.github.com>
2019-08-24 12:15:56 -0700

    Update Collatz-Conjecture.py

--- Collatz-Conjecture.py
+++ Collatz-Conjecture.py
@@ -5,5 +5,9 @@ for i in itertools.count(1):
     while i != 1:
         if i % 2 == 0:
             i = i / 2
-        else:
+        elif i % 2 != 0:
             i = i * 3 + 1
+        else:
+          print("{0} has failed the collatz conjecture. Possible solution discovered. ".format(i))
+          f= open("counterexample.txt","w+")
+          f.write("A possible counterexample has been found: " + i)
